f3c0afa6dcb59d81baf1e42197d478b1017d15e9
David Mvang <makouangoudavid@gmail.com>
2025-12-11 22:57:29 +0100

    display loader data - Fixes #8

--- app/routes/projects/index.tsx
+++ app/routes/projects/index.tsx
@@ -1,6 +1,8 @@
+import ProjectCard from "~/components/ProjectCard";
 import type { Route } from "./+types/index";
+import type { Project } from "~/type";
 
-export async function loader({request}: Route.LoaderArgs):Promise<any> {
+export async function loader({request}: Route.LoaderArgs):Promise<{projects: Project[]}> {
         const res = await fetch('http://localhost:8000/projects');
         const data = await res.json();
 
@@ -8,7 +10,7 @@ export async function loader({request}: Route.LoaderArgs):Promise<any> {
     }
 
 const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
-   const {projects} = loaderData;
+   const {projects} = loaderData as {projects: Project[]};
    console.log(projects);
 
     return ( 
@@ -16,6 +18,12 @@ const ProjectsPage = ({loaderData}: Route.ComponentProps) => {
             <h2 className="text-3xl text-white font-bold mb-8">
                 🚀 Projects
             </h2>
+
+            <div className="grid gap-6 sm:grid-cols-2">
+                {projects.map((project) => (
+                    <ProjectCard key={project.id} project={project} />
+                ))}
+            </div>
         </section>
      );
 }
